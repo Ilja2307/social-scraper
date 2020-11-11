1718e02eb981ae29658f51c3fe15c298c9671c99
leoyn <leo@mixify.ga>
2020-11-11 02:29:45 +0100

    Add users dir to packages in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='social_scraper',
     packages=["social_scraper", "social_scraper.collections",
-              "social_scraper.items", "social_scraper.platforms"],
+              "social_scraper.items", "social_scraper.platforms", "social_scraper.users"],
     description='Wrapper for snscrape',
     version='0.1',
     url='https://github.com/leoyn/social-scraper',
